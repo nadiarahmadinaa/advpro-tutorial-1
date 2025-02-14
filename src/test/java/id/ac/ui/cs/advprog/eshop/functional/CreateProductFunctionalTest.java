53012c5fcf2cde1013d3110f3502172d45af3fa0
nadiarahmadinaa <nadiarahmadinaaristawati@gmail.com>
2025-02-14 10:16:01 +0700

    Create CreateProductFunctionalTest.java

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -0,0 +1,52 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+public class CreateProductFunctionalTest {
+
+    @LocalServerPort
+    private int serverPort;
+
+    @Value("${app.baseUrl:http://localhost}")
+    private String testBaseUrl;
+    private String baseUrl;
+
+    @BeforeEach
+    void setUpTest() {
+        baseUrl = String.format("%s:%d", testBaseUrl, serverPort);
+    }
+
+    @Test
+    void user_can_create_product(ChromeDriver driver) {
+        driver.get(baseUrl + "/product/list");
+
+        driver.findElement(By.linkText("Create Product")).click();
+
+        WebElement nameInput = driver.findElement(By.id("nameInput"));
+        WebElement quantityInput = driver.findElement(By.id("quantityInput"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        nameInput.sendKeys("Test Product");
+        quantityInput.sendKeys("10");
+        submitButton.click();
+
+        driver.get(baseUrl + "/product/list");
+        String pageSource = driver.getPageSource();
+        assertTrue(pageSource.contains("Test Product"));
+        assertTrue(pageSource.contains("10"));
+    }
+}
