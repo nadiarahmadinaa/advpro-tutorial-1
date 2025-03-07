46ca39243af389aebc9571f5899e8297089f005d
nadiarahmadinaa <nadiarahmadinaaristawati@gmail.com>
2025-03-07 17:45:35 +0700

    [REFACTOR] Apply PaymentMethod and PaymentStatus enum check to Payment model tests

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -13,24 +15,27 @@ class PaymentTest {
     void testCreatePayment() {
         Map<String, String> paymentData = new HashMap<>();
         paymentData.put("voucherCode", "ESHOP1234ABC5678");
-        Payment payment = new Payment("VOUCHER", paymentData);
+        Payment payment = new Payment(PaymentMethod.VOUCHER, paymentData);
 
         assertNotNull(payment.getId());
-        assertEquals("VOUCHER", payment.getMethod());
-        assertEquals("PENDING", payment.getStatus());
+        assertEquals(PaymentMethod.VOUCHER, payment.getMethod());
+        assertEquals(PaymentStatus.PENDING, payment.getStatus());
         assertEquals("ESHOP1234ABC5678", payment.getPaymentData().get("voucherCode"));
     }
 
     @Test
     void testSetValidStatus() {
-        Payment payment = new Payment("BANK_TRANSFER", new HashMap<>());
-        payment.setStatus("SUCCESS");
-        assertEquals("SUCCESS", payment.getStatus());
+        Payment payment = new Payment(PaymentMethod.BANK_TRANSFER, new HashMap<>());
+        payment.setStatus(PaymentStatus.SUCCESS);
+        assertEquals(PaymentStatus.SUCCESS, payment.getStatus());
     }
 
     @Test
     void testSetInvalidStatusThrowsException() {
-        Payment payment = new Payment("BANK_TRANSFER", new HashMap<>());
-        assertThrows(IllegalArgumentException.class, () -> payment.setStatus("INVALID_STATUS"));
+        Payment payment = new Payment(PaymentMethod.BANK_TRANSFER, new HashMap<>());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            payment.setStatus(PaymentStatus.valueOf("INVALID_STATUS"));
+        });
     }
 }
